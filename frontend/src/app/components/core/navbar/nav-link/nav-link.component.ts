45ee42250badc5328e558726b75af1218f6d7892
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2023-08-23 11:47:38 -0700

    Added hover animations for navLinks

--- frontend/src/app/components/core/navbar/nav-link/nav-link.component.ts
+++ frontend/src/app/components/core/navbar/nav-link/nav-link.component.ts
@@ -1,29 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, HostBinding } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-link',
   templateUrl: './nav-link.component.html',
-  styleUrls: ['./nav-link.component.scss']
+  styleUrls: ['./nav-link.component.scss'],
+  animations: [
+    trigger('hoverOnOff', [
+      state('on', style({
+        backgroundColor: '#fff',
+      })),
+      state('off', style({
+      })),
+      transition('on => off', [
+        animate('0.05s')
+      ]),
+      transition('off => on', [
+        animate('0.1s')
+      ]),
+    ]),
+    trigger('hoverOnOffChild', [
+      state('on', style({
+        color: 'black',
+      })),
+      state('off', style({
+      })),
+      transition('on => off', [
+        animate('0.05s')
+      ]),
+      transition('off => on', [
+        animate('0.1s')
+      ]),
+    ]),
+  ],
 })
 export class NavLinkComponent {
   @Input() navIcon: string;
   @Input() navLink: string | undefined;
   @Input() navName: string;
 
-  hoverBox: boolean;
+  isOver: boolean;
 
   constructor() {
     this.navIcon = "";
     this.navLink = "";
     this.navName = "";
-    this.hoverBox = false;
-  }
-
-  test(): void {
-    console.log(`Mouse over ${this.navLink}`);
-  }
-
-  test2(): void {
-    console.log(`Mouse out ${this.navLink}`);
+    this.isOver = false;
   }
 }
