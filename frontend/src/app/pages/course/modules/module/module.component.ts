81f1eb0d17abfabf4d15ed11b07cc5257a3d4530
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2023-09-07 13:12:48 -0700

    Styled and animated individual modules, added assignments to modules

--- frontend/src/app/pages/course/modules/module/module.component.ts
+++ frontend/src/app/pages/course/modules/module/module.component.ts
@@ -1,5 +1,6 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger, query, sequence, stagger } from '@angular/animations';
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { Module } from 'src/app/models';
 import { ModuleEmpty } from 'src/app/models/httpResponseModels';
 
@@ -10,21 +11,65 @@ import { ModuleEmpty } from 'src/app/models/httpResponseModels';
   animations: [
     trigger('rotate', [
       state('default', style({ transform: 'rotate(0)' })),
-      state('rotated', style({ transform: 'rotate(90deg)' })),
-      transition('rotated <=> default', animate('400ms ease-in-out'))
+      state('rotated', style({ transform: 'rotate(-90deg)' })),
+      transition('rotated <=> default', animate('250ms ease-in-out'))
+    ]),
+    // Drop-down menu animation by Nichola Alkhouri
+    // https://stackblitz.com/edit/drop-down-menu-animation
+    trigger("moduleAnimation", [
+      transition(":enter", [
+        style({ height: 0, overflow: "hidden" }),
+        query(".assignment", [
+          style({ opacity: 0, transform: "translateY(-50px)" })
+        ]),
+        sequence([
+          animate("200ms", style({ height: "*" })),
+          query(".assignment", [
+            stagger(50, [
+              animate("400ms ease", style({ opacity: 1, transform: "none" }))
+            ])
+          ])
+        ])
+      ]),
+      transition(":leave", [
+        style({ height: "*", overflow: "hidden" }),
+        query(".assignment", [style({ opacity: 1, transform: "none" })]),
+        sequence([
+          query(".assignment", [
+            stagger(50, [
+              animate(
+                "350ms ease",
+                style({ opacity: 0, transform: "translateY(-50px)" })
+              )
+            ])
+          ]),
+          animate("200ms ease-in-out", style({ height: 0 }))
+        ])
+      ])
     ])
   ],
 })
 export class ModuleComponent {
   @Input() module: Module;
+  @Input() courseId: number;
   moduleExpanded: boolean;
 
-  constructor() {
+  constructor(private router: Router) {
     this.module = ModuleEmpty;
+    this.courseId = 0;
     this.moduleExpanded = false;
   }
 
   onClick() {
-    this.moduleExpanded = !this.moduleExpanded;
+    if (this.module.assignments.length > 0)
+      this.moduleExpanded = !this.moduleExpanded;
+  }
+
+  onAssignmentClick(assignmentId: number) {
+    this.router.navigateByUrl(`/courses/${this.courseId}/assignments/${assignmentId}`)
+  }
+
+  convertDateToString(date: Date): string {
+    return date.toLocaleString();
   }
 }
