f0970ee4dd0ba8708c1dd8b250f834cf389527ce
Jared-Velasquez <107715384+Jared-Velasquez@users.noreply.github.com>
2023-09-17 14:45:29 -0700

    Implemented username and password checking for login and signup screens

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -3,7 +3,7 @@ import { LoginService } from './../../services/login.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpStatusCode } from '@angular/common/http';
-import { Store } from '@ngrx/store';
+import { Store, USER_RUNTIME_CHECKS } from '@ngrx/store';
 import { AppState } from 'src/app/store/models/edifyState';
 import { UserActionTypes } from 'src/app/store/models/actionTypes';
 
@@ -14,6 +14,10 @@ import { UserActionTypes } from 'src/app/store/models/actionTypes';
 })
 export class LoginComponent implements OnInit, OnDestroy {
   badCredentials: boolean;
+  missingUsername: boolean;
+  badUsername: boolean;
+  missingPassword: boolean;
+  badPassword: boolean;
   loginSubscription: Subscription;
 
   constructor(
@@ -21,6 +25,10 @@ export class LoginComponent implements OnInit, OnDestroy {
     private loginService: LoginService,
   ) { 
     this.badCredentials = false;
+    this.missingUsername = false;
+    this.badUsername = false;
+    this.missingPassword = false;
+    this.badPassword = false;
     this.loginSubscription = Subscription.EMPTY;
   }
 
@@ -29,6 +37,17 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   onLogin(username: string, password: string) {
+    if (!username) this.missingUsername = true; else this.missingUsername = false;
+    if (!password) this.missingPassword = true; else this.missingPassword = false;
+
+    // Check username
+    const usernameExpression = new RegExp("^[a-z0-9]{2,15}$");
+    if (!usernameExpression.test(username)) this.badUsername = true; else this.badUsername = false;
+    // Check password
+    const passwordExpression = new RegExp("^[a-zA-Z0-9!@#$%^&*().,]{6,}$");
+    if (!passwordExpression.test(password)) this.badPassword = true; else this.badPassword = false;
+
+    if (this.missingUsername || this.missingPassword || this.badUsername || this.badPassword) return;
     this.loginSubscription = this.loginService.login(username, password).subscribe({
       next: (response) => {
         if (!response) {
--- frontend/src/app/pages/sign-up/sign-up.component.ts
+++ frontend/src/app/pages/sign-up/sign-up.component.ts
@@ -15,6 +15,12 @@ export class SignUpComponent implements OnDestroy {
   chosenRole: string;
   badCredentials: boolean;
   loginSubscription: Subscription;
+  missingUsername: boolean;
+  badUsername: boolean;
+  missingPassword: boolean;
+  badPassword: boolean;
+  missingFirstName: boolean;
+  missingLastName: boolean;
 
   constructor(private loginService: LoginService, private router: Router) {
     this.menu = {
@@ -24,9 +30,28 @@ export class SignUpComponent implements OnDestroy {
     this.chosenRole = "STUDENT";
     this.badCredentials = false;
     this.loginSubscription = Subscription.EMPTY;
+    this.missingUsername = false;
+    this.badUsername = false;
+    this.missingPassword = false;
+    this.badPassword = false;
+    this.missingFirstName = false;
+    this.missingLastName = false;
   }
 
   onSignup(username: string, password: string, firstName: string, lastName: string, role: string) {
+    if (!username) this.missingUsername = true; else this.missingUsername = false;
+    if (!password) this.missingPassword = true; else this.missingPassword = false;
+    if (!firstName) this.missingFirstName = true; else this.missingFirstName = false;
+    if (!lastName) this.missingLastName = true; else this.missingLastName = false;
+
+    // Check username
+    const usernameExpression = new RegExp("^[a-z0-9]{2,15}$");
+    if (!usernameExpression.test(username)) this.badUsername = true; else this.badUsername = false;
+    // Check password
+    const passwordExpression = new RegExp("^[a-zA-Z0-9!@#$%^&*().,]{6,}$");
+    if (!passwordExpression.test(password)) this.badPassword = true; else this.badPassword = false;
+
+    if (this.missingUsername || this.missingPassword || this.missingFirstName || this.missingLastName || this.badUsername || this.badPassword) return;
     this.loginSubscription = this.loginService.signup(username, password, firstName, lastName, role).subscribe({
       next: (response) => {
         if (!response) {
